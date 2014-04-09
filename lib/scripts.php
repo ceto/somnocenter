ec0d61e87efe33a9821dbfe30e65fc268748f843
Szabó Gábor <szabogabi@gmail.com>
2014-04-09 14:38:32 +0200

    bottom sidebar tweak

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2057b4a193b69a240c77fd213f097a17');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '797ae2527fb77512e98eca49da6532d9');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
