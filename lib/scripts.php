24bcec0ba88094836940e55217bd9915b651ded3
Szabó Gábor <szabogabi@gmail.com>
2014-04-22 15:25:06 +0200

    toprow tweak

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2c36a758beab1123bb0d7235ddeeaf51');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ab633c2b25324977930cb5dd092d7093');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
