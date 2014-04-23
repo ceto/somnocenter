7156efef6e783cf41c9177298814141f590dc298
Szabó Gábor <szabogabi@gmail.com>
2014-04-23 15:05:31 +0200

    heading size fix

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '12480242626ed3fb16667da2e62d3579');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5a372ad3d11b4c052e17f2d4fb666ede');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
