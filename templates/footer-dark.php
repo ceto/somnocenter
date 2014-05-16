c69781422f89e3adde2090b0acb18c143faba255
Szabó Gábor <szabogabi@gmail.com>
2014-05-16 12:48:44 +0200

    footer signup mailchimp off

--- templates/footer-dark.php
+++ templates/footer-dark.php
@@ -11,24 +11,27 @@
       </p>
       <a class="catmore" href="<?php echo get_permalink(1182); ?>">Hogyan juthat hozzá</a>
     </section>
-    <section class="newsletter-block">
-      <!-- Begin MailChimp Signup Form -->
-        <div id="mc_embed_signup">
-        <form action="http://somnocenter.us8.list-manage.com/subscribe/post?u=777cff0d0124c922c39447d01&amp;id=98f01fad7f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-          <h3>Alvás hírlevél havonta</h3>
-          <p>Hírlevelünkből értesülhet akcióinkról, alvással kapcsolatos írásainkról</p>
-          <div class="mc-field-group">
-            <label for="mce-EMAIL">E-mail cím </label>
-            <input type="email" value="" placeholder="Adja meg e-mail címét" name="EMAIL" class="required email" id="mce-EMAIL">
-            <input type="submit" value="Mehet" name="subscribe" id="mc-embedded-subscribe" class="button btn">
+    <?php if (!is_page(1182)): ?>
+      <section class="newsletter-block">
+        <!-- Begin MailChimp Signup Form -->
+          <div id="mc_embed_signup">
+          <form action="http://somnocenter.us8.list-manage.com/subscribe/post?u=777cff0d0124c922c39447d01&amp;id=98f01fad7f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <h3>Alvás hírlevél havonta</h3>
+            <p>Hírlevelünkből értesülhet akcióinkról, alvással kapcsolatos írásainkról</p>
+            <div class="mc-field-group">
+              <label for="mce-EMAIL">E-mail cím </label>
+              <input type="email" value="" placeholder="Adja meg e-mail címét" name="EMAIL" class="required email" id="mce-EMAIL">
+              <input type="submit" value="Mehet" name="subscribe" id="mc-embedded-subscribe" class="button btn">
+            </div>
+            <div id="mce-responses" class="clear">
+              <div class="response" id="mce-error-response" style="display:none"></div>
+              <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;"><input type="text" name="b_777cff0d0124c922c39447d01_98f01fad7f" value=""></div>
+          </form>
           </div>
-          <div id="mce-responses" class="clear">
-            <div class="response" id="mce-error-response" style="display:none"></div>
-            <div class="response" id="mce-success-response" style="display:none"></div>
-          </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-          <div style="position: absolute; left: -5000px;"><input type="text" name="b_777cff0d0124c922c39447d01_98f01fad7f" value=""></div>
-        </form>
-        </div>
-    </section>
+      </section>
+    <?php endif; ?>
+
   </div>
 </aside>
