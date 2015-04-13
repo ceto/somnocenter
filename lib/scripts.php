e9b0c7b89076575a0b860c14664b2e56d3f41457
Gabor Szabo <szabogabi@gmail.com>
2015-04-13 13:43:05 +0200

    gallery thumbnail inline-block bug

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2a79cd97dedc441f039bea199a1227d1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '534867f13441823b2da4a4b3456f8950');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
