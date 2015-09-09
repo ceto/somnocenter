14ca90fb8c8198d47aa2ce0982a0187f089fb3fc
Gabor Szabo <szabogabi@gmail.com>
2015-09-09 13:57:33 +0200

    telefonfent [Deploy: Live]

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '170145a64370aecbd5fd97fbc6511408');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '706c5743ca66c396a174cf292024d6fc');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
