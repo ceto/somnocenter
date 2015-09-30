006a78afcc179a00826bf342d50db0964f6c92e6
Gabor Szabo <szabogabi@gmail.com>
2015-09-30 13:41:11 +0200

    Regdl template fix2 [Deploy: Live]

--- lib/scripts.php
+++ lib/scripts.php
@@ -33,7 +33,7 @@ function roots_scripts() {
 
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '1d6ad313fc99eac3c70afbdb3e98857c', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '573b91ad17cd3938cfec99780fce63c1', true);
   
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
