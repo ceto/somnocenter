aa8a56f38b36eb4965a4736dce09422ebffc3815
Gabor Szabo <szabogabi@gmail.com>
2015-10-01 11:43:53 +0200

    title subtitle switch

--- page-registereddl.php
+++ page-registereddl.php
@@ -23,11 +23,10 @@ Template Name: Letöltés Sablon Orvosoknak
           <?php the_post_thumbnail('medium21'); ?>
         </figure>
       <?php endif; ?>
+      <h1 class="survey-title"><?php the_title(); ?></h1>
       <?php if (get_post_meta( $post->ID, '_cmb_subtitle', true )!='' ): ?>
         <h2><?php echo do_shortcode(get_post_meta( $post->ID, '_cmb_subtitle', true ) ); ?></h2>
       <?php endif; ?>
-      <h1 class="survey-title"><?php the_title(); ?></h1>
-
 
       <div class="survey-content">
         <?php the_content(); ?>
