1be77f838d14c8cb5f1b77117834320bb2f08ad9
Gabor Szabo <szabogabi@gmail.com>
2015-12-01 17:40:49 +0100

    urlap jav3 [Deploy: Live]

--- templates/contact-form.php
+++ templates/contact-form.php
@@ -100,7 +100,7 @@ if(!$human == 0){
       <select id="message_center" name="message_center">
         <option value="0"><?php _e('Válasszon központot','roots'); ?></option>
         <option value="Budapest">Budapest</option>
-        <?php if ($page->ID!=2426) :?>
+        <?php if (get_the_id()!=2426) :?>
         <option value="Pécs" disabled="disabled">Pécs - Csak telefonon (+36 30 476 1400)</option>
         <option value="Szeged">Szeged</option>
         <?php endif; ?>
