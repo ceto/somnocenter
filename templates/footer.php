cc1be083e2031b635e8938484268954cc4c4c023
Gabor Szabo <szabogabi@gmail.com>
2015-12-09 18:11:15 +0100

    footerjav [Deploy: Live]

--- templates/footer.php
+++ templates/footer.php
@@ -2,9 +2,10 @@
   <div class="row">
     <?php // dynamic_sidebar('sidebar-footer'); ?>
     <p>
-      &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> | 
-      <?php _e('Bejelentkezés:','roots'); ?> <a href="tel:0036205007993" class="telcsi">+36 20 500 7993</a> | 
-      <?php _e('Honlap és Arculat','roots'); ?> <a href="http://hydrogene.hu" target="_blank">Hydrogene</a></p>
+      &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> &middot; Minden jog fenntartva &middot;
+      <?php _e('Időpontfoglalás:','roots'); ?> <a href="tel:0036205007993" class="telcsi">+36 20 500 7993</a> <br>
+      <?php _e('Kommunikáció és sajtókapcsolat:','roots'); ?> <a href="mailto:kommunikacio@somnocenter.hu">kommunikacio@somnocenter.hu</a> &middot;
+      <?php _e('Honlap és Arculat','roots'); ?> <a href="http://hydrogene.hu/referencia/egeszsegugyi-kozpont-teljes-arculat-es-honlap/" target="_blank">Hydrogene</a></p>
   </div>
 </footer>
 
