dd900fd850f33748ffbc8978d9fd72bbf960c074
Gabor Szabo <szabogabi@gmail.com>
2016-01-29 16:39:03 +0100

    Email hidden input reset off and js rebuild [Deploy: Live]

--- lib/scripts.php
+++ lib/scripts.php
@@ -33,7 +33,7 @@ function roots_scripts() {
 
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '2c2c92beb7b649e6f62b7c7f43cf1fdb', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '7e3ea446604e7c76171d5c2e280989fc', true);
   
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
