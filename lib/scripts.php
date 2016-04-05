df4fbf7d9516f1c4041166e5c41c59f003fec12e
Gabor Szabo <szabogabi@gmail.com>
2016-04-05 11:23:09 +0200

    Collapse vissza

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '521b7ed444c534afdf4c3d2ec5bb8965');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '75ba07e7c1dc09bfd741c8a3c85e3680');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
