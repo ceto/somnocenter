b8b5e172a5c2578e667f5ce6a4436ef2b38aa44e
Gabor Szabo <szabogabi@gmail.com>
2016-04-07 11:51:59 +0200

    sidebar added to pages

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2d4115b21684611b74c4d2fea9054713');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7b1cff0c45f74a282441dac6eac9dfe3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content-page.php
+++ templates/content-page.php
@@ -4,8 +4,14 @@
       <?php the_real_excerpt(); ?>
     </div>
   </section>
-  <section class="ps ps--narrow pagecopy">
-  <?php the_content('', TRUE); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+  <div class="ps">
+  <section class="pagecopy">
+    <?php the_content('', TRUE); ?>
+    <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
   </section>
+  <aside class="pagesidebar inner-side"  role="complementary">
+     <?php get_template_part( 'templates/sidebar', 'inner') ?>
+  </aside>
+
+  </div>
 <?php endwhile; ?>
