0ce37f118ec49f73d126978fcd8f577a47438d4d
Gabor Szabo <szabogabi@gmail.com>
2016-09-07 00:51:18 +0200

    telefon [Deploy: Live]

--- templates/footer.php
+++ templates/footer.php
@@ -4,7 +4,7 @@
     <p>
       &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> &middot; Minden jog fenntartva &middot; <a href="<?= get_permalink(2837); ?>">Jogi nyilatkozat</a> &middot; <a href="<?= get_permalink(2843); ?>">Adatok védelme</a> &middot;
       <?php _e('Időpontfoglalás:','roots'); ?> <a href="tel:0036205007993" class="telcsi">+36 20 500 7993</a> <br>
-      <?php _e('Kommunikáció és sajtókapcsolat:','roots'); ?> <a href="mailto:kommunikacio@somnocenter.hu">kommunikacio@somnocenter.hu</a> - <a href="tel:0036209430848">+36 20 943 0848</a> &middot;
+      <?php _e('Kommunikáció és sajtókapcsolat:','roots'); ?> <a href="mailto:kommunikacio@somnocenter.hu">kommunikacio@somnocenter.hu</a> vagy <a href="tel:0036209430848">+36 20 943 0848</a> &middot;
       <?php _e('Honlap és Arculat:','roots'); ?> <a href="http://hydrogene.hu/referencia/egeszsegugyi-kozpont-teljes-arculat-es-honlap/" target="_blank">Hydrogene</a></p>
   </div>
 </footer>
