be213536655bfe24977060efbbabdf3653449de5
Gábor Szabó <szabogabi@gmail.com>
2021-12-02 13:48:43 +0100

    telefonszámok

--- templates/footer.php
+++ templates/footer.php
@@ -6,12 +6,11 @@
       </p>
     <p class="footer__copy">
       &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> &middot; Minden jog fenntartva &middot; <a href="<?= get_permalink(2837); ?>">Jogi nyilatkozat</a> &middot; <a href="<?= get_permalink(2843); ?>">Adatok védelme</a> &middot;
-      <?php _e('Időpontfoglalás:','roots'); ?> <a href="tel:0036205007993" class="telcsi">+36 20 500 7993</a> <br>
-      <?php _e('Kommunikáció és sajtókapcsolat:','roots'); ?> <a href="mailto:kommunikacio@somnocenter.hu">kommunikacio@somnocenter.hu</a> vagy <a href="tel:0036209430848">+36 20 943 0848</a> &middot;
+      <?php _e('Időpontfoglalás:','roots'); ?> <a href="tel:0036203443434" class="telcsi">+36 20 344 3434</a> <br>
+      <?php _e('Kommunikáció és sajtókapcsolat:','roots'); ?> <a href="mailto:kommunikacio@somnocenter.hu">kommunikacio@somnocenter.hu</a> vagy <a href="tel:003614878065">+36 1 487 8065</a> &middot;
       <?php _e('Honlap és Arculat:','roots'); ?> <a href="http://hydrogene.hu/referencia/egeszsegugyi-kozpont-teljes-arculat-es-honlap/" target="_blank">Hydrogene</a></p>
 
   </div>
 </footer>
 
 <?php wp_footer(); ?>
-
